0a23ff33617cf2b987c71ab52a214b43ad234bcf
JUNE <jh010303@naver.com>
2024-04-06 21:53:43 +0900

    posts migration

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,5 @@ class Post(models.Model): # 일 대 다 에서 다쪽, PK를 가지고 있음
     title = models.CharField(max_length=50,null=True)# 보통 많이 사용
     content = models.CharField(max_length=100,null=True,blank=True) # 빈칸 허용
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name = 'posts',null=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
