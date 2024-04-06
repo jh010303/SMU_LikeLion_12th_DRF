68ae19cbac0f24541d81ce7e2526006d2344141b
JUNE <jh010303@naver.com>
2024-04-06 21:53:50 +0900

    users migration

--- users/models.py
+++ users/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # models를 사용하면 id는 자동 생성
 class User(AbstractUser):
-    pass
+    introducing = models.CharField(max_length=10,null=True,blank=True)
+    gender = models.CharField(max_length=2, null=True)
+
 
