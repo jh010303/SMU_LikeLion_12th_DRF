520af3f5492779323b3906953b7f052d58dc3b70
202010850 김준형 <jh010303@naver.com>
2024-04-10 21:04:31 +0900

    시니얼라이저

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from users.models import User
+from posts.models import Post
+
+class UserSerializer(serializers.ModelSerializer):
+    posts_num = serializers.SerializerMethodField() # 파라미터에 옵션 read_only, write_only
+
+    class Meta:
+        model = User
+        fields = ['id','username','email', 'password', 'posts_num'] # '__all__' 모든 필드를 시리얼라이징 할 때 
+        extra_kwargs = {'password':{'write_only':True}}
+    
+    def get_posts_num(self,obj):
+        return Post.objects.filter(user=obj).count()
+
+
+    
