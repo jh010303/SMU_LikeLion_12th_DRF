961ce4977185a9fb209fa5f0b459f30f09671826
JUNE <jh010303@naver.com>
2024-05-06 01:36:19 +0900

    views

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,53 @@
-from django.shortcuts import render
 from posts.models import Post
-# Create your views here.
+from posts.serializers import PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-def post_list(re)
+# Create your views here.
+# 게시글 수정, 게시글 삭제
+
+@api_view(['GET','POST'])
+def post_list_api_view(request): # post 목록 조회
+    if request.method == 'GET': # 요청된 정보를 검색하여 응답해줌 ( read )
+        posts = Post.objects.all() # 포스트 전체
+        serializer = PostSerializer(posts,many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST': # 요청된 자원을 생성(create)함
+        serializer = PostSerializer(data=request.data,user=request.user) # 요청 데이터에 기반하여 PostSerializer 생성
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def post_retrieve_api_view(request,pk): # post 특정 목록 조회 (pk통해서 구분)
+    # pk를 가진 post가 존재하는지 확인
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method =='GET': # ( 특정 게시글 read )
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT': # 요청된 자원을 수정(update)함
+        serializer = PostSerializer(post,data=request.data,user=request.user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'DELETE': # 특정 게시글 삭제(delete)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
+    
+        
+
+    
--- users/views.py
+++ users/views.py
@@ -1,4 +1,77 @@
-from django.shortcuts import render
+from users.models import User 
+from users.serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+# 게시글 수정, 게시글 삭제
+    
+@api_view(['GET'])
+def user_login_api_view(request): # user가 login 하는 view 함수
+    if request.method == 'GET': # 데이터를 읽어야 함
+        email = request.data.get('email') # email 입력 데이터 ( 로그인 할 때 사용 )
+        
+        if email is None: # email이 존재하지 않으면
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        #이메일이 존재하면
+        password = request.data.get('password') # password 입력 데이터
+        user = User.objects.get(email=email) # 입력한 email을 가지고 있는 unique한 user model을 불러옴
+        # 비밀번호가 맞으면
+        if user.check_password(password):
+            serializer = UserSerializer(user) # 요청하는 data에 대응하는 user 가져옴
+            serializer.save() # 저장
+            return Response(serializer.data) # user 반환
+        
+        # 비밀번호가 맞지 않으면
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['POST'])
+def user_join_api_view(request): # user가 회원가입 하는 함수
+    if request.method == 'POST': # 요청한 데이터를 생성해야 함
+        # 회원가입에 필요한 데이터 가져옴 
+        id = request.data.get('id')
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        introducing = request.data.get('introdcuing')
+        gender = request.data.get('gender')
+        # 위의 데이터를 바탕으로 새로운 user model 생성
+        new_user = User.objects.create_user(
+            id=id,
+            username=username,
+            email = email,
+            password = password,
+            introducing = introducing,
+            gender = gender
+        )
+        serializer = UserSerializer(new_user) # 새로운 user 시리얼화
+        serializer.save() # 저장
+        return Response(serializer.data,status=status.HTTP_201_CREATED) # 새로운 user 반환
+
+@api_view(['DELETE'])
+def user_delete_api_view(request,pk): #user 삭제 ( 탈퇴 개념 )
+    try:
+        delete_user = User.objects.get(pk=pk)
+    except delete_user.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE': 
+        delete_user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+        
+        
+
+
+        
+
+
+
+
+    
+        
+
+    
